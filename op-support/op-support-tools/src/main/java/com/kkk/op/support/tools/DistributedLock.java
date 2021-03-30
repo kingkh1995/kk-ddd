67d816c9ee22bf02859cd06ce62550c0411d9280
KaiKoo <kingkh1995@outlook.com>
2021-03-30 23:49:29 +0800

    DistributedLock commit

---
+++ op-support/op-support-tools/src/main/java/com/kkk/op/support/tools/DistributedLock.java
@@ -0,0 +1,111 @@
+package com.kkk.op.support.tools;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import javax.validation.constraints.NotBlank;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+
+/**
+ * 分布式锁实现
+ * @author KaiKoo
+ */
+@Slf4j
+public class DistributedLock {
+
+    private final StringRedisTemplate redisTemplate;
+
+    private final ThreadLocal<Map<String, String>> context = ThreadLocal.withInitial(HashMap::new);
+
+    @Setter
+    private int maxRetryTimes = 3;
+
+    @Setter
+    private long sleepInterval = 500L;
+
+    @Setter
+    private long lockTime = 10L;
+
+    @Setter
+    private TimeUnit lockTimeUnit = TimeUnit.SECONDS;
+
+    public DistributedLock(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    // 睡眠时间递增，并且取随机值，防止雪崩
+    private long generateSleepMills(int i) {
+        var interval = sleepInterval << i;
+        return ThreadLocalRandom.current()
+                .nextLong((long) (interval * 0.8), (long) (interval * 1.2));
+    }
+
+    // 生成 UUID 作为 RequestId 并保存在 ThreadLocal 中 释放锁的时候再取出值做对比
+    private String generateRequestId(String key) {
+        String uuid = UUID.randomUUID().toString();
+        context.get().put(key, uuid);
+        return uuid;
+    }
+
+    private String getRequestId(String key) {
+        return context.get().get(key);
+    }
+
+    private boolean lock(String key, String requestId) {
+        return redisTemplate.opsForValue().setIfAbsent(key, requestId, lockTime, lockTimeUnit);
+    }
+
+    /**
+     * 获取锁，失败不重试
+     */
+    public boolean tryLock(String key) {
+        return this.tryLock(key, 0);
+    }
+
+    /**
+     * 获取锁，失败则自旋重试
+     */
+    public boolean tryLock(@NotBlank String key, int retry) {
+        var locked = false;
+        var i = 0;
+        retry = retry > maxRetryTimes ? maxRetryTimes : retry;
+        var requestId = generateRequestId(key);
+        locked = this.lock(key, requestId);
+        while (!locked && retry > 0) {
+            try {
+                Thread.sleep(generateSleepMills(i++));
+            } catch (InterruptedException e) {
+                log.error(String.format("线程【%s】睡眠被中断！", Thread.currentThread().getName()), e);
+            }
+            locked = this.lock(key, requestId);
+            retry--;
+        }
+        return locked;
+    }
+
+    private final static String SCRIPT =
+            "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+
+    /**
+     * 使用lua脚本进行原子解锁
+     *
+     * 解决问题：如果锁已自动释放请求仍未执行完，则可能会让被其他线程获取到该锁，必须保证不能释放掉别人加的锁
+     * （同时业务逻辑中也要通过乐观锁或其他方式避免并发问题发生）
+     */
+    public boolean unlock(@NotBlank String key) {
+        var requestId = getRequestId(key);
+        if (requestId == null) {
+            return false;
+        }
+        Long result = redisTemplate
+                .execute(new DefaultRedisScript<>(SCRIPT, Long.class), Arrays.asList(key),
+                        Arrays.asList(requestId));
+        return result > 0;
+    }
+}
